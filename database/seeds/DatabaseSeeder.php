2eeefe6fd7d0f12e2c4135971f7acec5a34c8b54
Mauritz Erick <mauritz.erick@gmail.com>
2021-06-10 15:54:44 +1000

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,5 +22,38 @@ class DatabaseSeeder extends Seeder
             'start_from'=>'2021-06-01',
             'image'=>'avatar2.png'
         ]);
+
+        // User::create([
+        //     'name'=>'User 2',
+        //     'email'=>'2@gmail.com',
+        //     'password'=>bcrypt('password'),
+        //     'department_id'=>'2',
+        //     'role_id'=>'2',
+        //     'designation'=>'Sydney',
+        //     'start_from'=>'2021-06-02',
+        //     'image'=>'avatar2.png'
+        // ]);
+
+         // User::create([
+        //     'name'=>'User 3',
+        //     'email'=>'3@gmail.com',
+        //     'password'=>bcrypt('password'),
+        //     'department_id'=>'3',
+        //     'role_id'=>'3',
+        //     'designation'=>'Melbourne',
+        //     'start_from'=>'2021-06-03',
+        //     'image'=>'avatar2.png'
+        // ]);
+
+         // User::create([
+        //     'name'=>'User 4',
+        //     'email'=>'4@gmail.com',
+        //     'password'=>bcrypt('password'),
+        //     'department_id'=>'4',
+        //     'role_id'=>'4',
+        //     'designation'=>'Melbourne',
+        //     'start_from'=>'2021-06-04',
+        //     'image'=>'avatar2.png'
+        // ]);
     }
 }
